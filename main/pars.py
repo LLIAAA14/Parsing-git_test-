5cd665836dbdb6d2f7129d894b14d0dbb259f83c
LLIAAA <blackholes001@gmail.com>
2024-06-22 13:30:29 +0300

    1

---
+++ main/pars.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_html(url):
+    house = requests.get(url)
+    return house.text
+
+def get_data(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    rooms = soup.find_all('div', class_='card')
+    
+for room in rooms:
+        
+
+def main():
+    url = 'https://kaliningrad.sutochno.ru/'
+    print(get_data(get_html(url)))
+
+if __name__ == '__main__':
+    main()
+
